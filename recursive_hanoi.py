ff737c889c336e21a87810f3fa66e3386c45a05e
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-12-14 19:33:43 +0100

    Reformat output

--- recursive_hanoi.py
+++ recursive_hanoi.py
@@ -1,9 +1,9 @@
-step = 1
+step = 0
 
 def move(fr, to):
     global step
-    print(f"Step {step}:\tMove from {fr} to {to}")
     step += 1
+    print(f"Step {step}:\tMove from {fr} to {to}")
 
 def hanoi(fr, to, via, n):
     if n == 0:
@@ -13,7 +13,7 @@ def hanoi(fr, to, via, n):
         move(fr, to)
         hanoi(via, to, fr, n-1)
 
-n = input("How many layers does your tower of Hanoi have?\n\t")
+n = input("\n\nHow many layers does your tower of Hanoi have?\t")
 try:
     n = int(n)
 except:
@@ -25,6 +25,8 @@ while type(n) is not int or n < 1:
     except:
         pass
 
-print("To solve the tower in the least possible moves, follow these instructions:\n\n")
+print("\nTo solve the tower in the least possible moves, follow these instructions:")
+input("(press Enter to start)\n")
 
 hanoi(1, 3, 2, n)
+print(f"\nCompleted in {step} steps.\n")
